806d5014f5e9726e6197534c280cf43b635fe891
Gus McCoy <51655110+gusmccoy@users.noreply.github.com>
2021-01-26 19:53:22 -0600

    Added Scripts Folder and NetworkController.cs

---
+++ Assets/Scripts/NetworkController.cs
@@ -0,0 +1,32 @@
+﻿using Photon.Pun;
+using Photon.Realtime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NetworkController : MonoBehaviourPunCallbacks
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("Connecting to server.");
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    // Update is called once per frame
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Hello World!");
+    }
+
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        print("Disconnected from server for reason " + cause.ToString());
+    }
+
+    void Update()
+    {
+
+    }
+}
