647d4ad0e8c75298b671c8bb1d15a4985344e2af
Gus McCoy <51655110+gusmccoy@users.noreply.github.com>
2021-01-29 16:10:57 -0600

    Added PH images

---
+++ Assets/Scripts/BoardManager.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardManager : MonoBehaviour
+{
+    public Sprite[] images;
+    public SpriteRenderer[] spriteRenderers;
+
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Reshuffle();
+        }
+    }
+
+    public void Reshuffle()
+    {
+        for (int i = 0; i < images.Length; i++)
+        {
+            Sprite tmp = images[i];
+            int r = Random.Range(i, images.Length);
+            images[i] = images[r];
+            images[r] = tmp;
+        }
+
+        for(int i = 0; i < spriteRenderers.Length; i++)
+        {
+            spriteRenderers[i].sprite = images[i];
+        }
+    }
+
+}
