489a7a361eb9fcb2fd97f69ffb94de64982e57c3
ManerdHumhordun <35775584+ManerdHumhordun@users.noreply.github.com>
2021-02-05 12:32:17 -0600

    Added Node Logic

--- Assets/Scripts/NodeController.cs
+++ Assets/Scripts/NodeController.cs
@@ -1,33 +1,66 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static GameObjectProperties;
 
 public class NodeController : MonoBehaviour
 {
-    public Sprite playerOne;
-    public Sprite playerTwo;
-    public Sprite unowned;
+    public Sprite playerOneSprite;
+    public Sprite playerTwoSprite;
+    public Sprite blankSprite;
+
+    public Node nodeEntity;
 
     void Start()
     {
-        ClaimNode(unowned);
+        // Mark Branch Unowned at start
+        ClaimNode(blankSprite);
     }
 
     private void OnMouseDown()
     {
-        if (Game.playerOneTurn)
+        // Determine if current player can place here. (As of now simply meaning you can't override
+        if (isNodeBlank())
         {
-            ClaimNode(playerOne);
+            nodeEntity.nodeState.ownerColor = nodeEntity.gameController.getCurrentPlayerColor();
+            nodeEntity.nodeState.nodeColor = nodeEntity.gameController.getCurrentPlayerColor();
+
+            // Change color
+            if (Game.playerOneTurn)
+                ClaimNode(playerOneSprite);
+            else
+                ClaimNode(playerTwoSprite);
         }
+        // Are you trying to undo a selection?
+        else if (isNodeColorOfCurrentPlayer())
+        {
+            ClaimNode(blankSprite);
+        }
+
+        // Print
+        print("You clicked on:\n" + this.GetComponent<Renderer>().name + ", ID: " + nodeEntity.id + ", Color: ");
+        if (GetComponent<SpriteRenderer>().sprite == playerOneSprite)
+            print("Orange\n");
+        else if (GetComponent<SpriteRenderer>().sprite == playerTwoSprite)
+            print("Purple\n");
         else
-        {
-            ClaimNode(playerTwo);
-        }
+            print("Blank\n");
     }
 
     private void ClaimNode(Sprite playerColor)
     {
         SpriteRenderer sprite = GetComponent<SpriteRenderer>();
         sprite.sprite = playerColor;
+    }
+
+    private bool isNodeBlank()
+    {
+        return nodeEntity.nodeState.ownerColor == PlayerColor.Blank;
+    }
+
+
+    private bool isNodeColorOfCurrentPlayer()
+    {
+        return nodeEntity.nodeState.nodeColor == nodeEntity.gameController.getCurrentPlayerColor();
     }
 }
