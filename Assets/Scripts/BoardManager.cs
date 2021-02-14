4dac3387256b325b315d4ea4917810d0b3538e95
Gus McCoy <51655110+gusmccoy@users.noreply.github.com>
2021-02-14 13:54:41 -0600

    Update GUI Logic

--- Assets/Scripts/BoardManager.cs
+++ Assets/Scripts/BoardManager.cs
@@ -34,7 +34,68 @@ public class BoardManager : MonoBehaviour
 
     public void SetSquareSpite(SquareState squareInfo, SpriteRenderer squareSprite)
     {
-
+        switch(squareInfo.resourceColor)
+        {
+            case SquareResourceColor.Red:
+                switch (squareInfo.resourceAmount)
+                {
+                    case SquareResourceAmount.One:
+                        squareSprite.sprite = images[0];
+                        break;
+                    case SquareResourceAmount.Two:
+                        squareSprite.sprite = images[1];
+                        break;
+                    case SquareResourceAmount.Three:
+                        squareSprite.sprite = images[2];
+                        break;
+                }
+                break;
+            case SquareResourceColor.Blue:
+                switch (squareInfo.resourceAmount)
+                {
+                    case SquareResourceAmount.One:
+                        squareSprite.sprite = images[3];
+                        break;
+                    case SquareResourceAmount.Two:
+                        squareSprite.sprite = images[4];
+                        break;
+                    case SquareResourceAmount.Three:
+                        squareSprite.sprite = images[5];
+                        break;
+                }
+                break;
+            case SquareResourceColor.Yellow:
+                switch (squareInfo.resourceAmount)
+                {
+                    case SquareResourceAmount.One:
+                        squareSprite.sprite = images[6];
+                        break;
+                    case SquareResourceAmount.Two:
+                        squareSprite.sprite = images[7];
+                        break;
+                    case SquareResourceAmount.Three:
+                        squareSprite.sprite = images[8];
+                        break;
+                }
+                break;
+            case SquareResourceColor.Green:
+                switch (squareInfo.resourceAmount)
+                {
+                    case SquareResourceAmount.One:
+                        squareSprite.sprite = images[9];
+                        break;
+                    case SquareResourceAmount.Two:
+                        squareSprite.sprite = images[10];
+                        break;
+                    case SquareResourceAmount.Three:
+                        squareSprite.sprite = images[11];
+                        break;
+                }
+                break;
+            case SquareResourceColor.Blank:
+                squareSprite.sprite = images[12];
+                break;
+        }
     }
 
 }
