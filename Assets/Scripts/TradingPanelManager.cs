5ce99999b52c92921d4e763f572335ea65fea1a2
Gus McCoy <51655110+gusmccoy@users.noreply.github.com>
2021-02-15 22:23:33 -0600

    Code Scaffolding

---
+++ Assets/Scripts/TradingPanelManager.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TradingPanelManager : MonoBehaviour
+{
+    public TMP_Text[] tilesSelected;
+
+    void Start()
+    {
+        
+    }
+
+    public void OnConfirmClick()
+    {
+
+    }
+
+    public void OnCancelClick()
+    {
+
+    }
+
+    public void OnRedClick()
+    {
+
+    }
+
+    public void OnBlueClick()
+    {
+
+    }
+
+    public void OnYellowClick()
+    {
+
+    }
+
+    public void OnGreenClick()
+    {
+
+    }
+}
