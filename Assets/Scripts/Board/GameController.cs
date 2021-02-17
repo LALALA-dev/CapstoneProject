1567b99d3a17736ba34fba8e85cf65fd7174c9c7
Gus McCoy <51655110+gusmccoy@users.noreply.github.com>
2021-02-17 16:27:00 -0600

    Update GameController.cs

--- Assets/Scripts/Board/GameController.cs
+++ Assets/Scripts/Board/GameController.cs
@@ -251,7 +251,6 @@ public class GameController
             GameInformation.playerOneScore += 2;
         else if (playerOneNetwork < playerTwoNetwork)
             GameInformation.playerTwoScore += 2;
-        Debug.Log("SCORES:\nPLAYER ONE: " + GameInformation.playerOneScore + "\nPLAYER TWO: " + GameInformation.playerTwoScore);
     }
 }
 
