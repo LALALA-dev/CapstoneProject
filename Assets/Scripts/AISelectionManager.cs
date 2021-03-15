abe2ff846e9664fb0e295fd47ee848b75d892671
Gus McCoy <51655110+gusmccoy@users.noreply.github.com>
2021-03-15 11:04:38 -0500

    Update AISelectionManager.cs

--- Assets/Scripts/AISelectionManager.cs
+++ Assets/Scripts/AISelectionManager.cs
@@ -37,25 +37,30 @@ public class AISelectionManager : MonoBehaviour
     public void OnHatSelect()
     {
         GameInformation.playerAvatar = "HAT";
+        selector.transform.position = new Vector3(avatars[0].transform.position.x, selector.transform.position.y);
     }
 
     public void OnShipSelect()
     {
         GameInformation.playerAvatar = "BATTLESHIP";
+        selector.transform.position = new Vector3(avatars[1].transform.position.x, selector.transform.position.y);
     }
 
     public void OnCarSelect()
     {
         GameInformation.playerAvatar = "CAR";
+        selector.transform.position = new Vector3(avatars[2].transform.position.x, selector.transform.position.y);
     }
 
     public void OnThimbleSelect()
     {
         GameInformation.playerAvatar = "THIMBLE";
+        selector.transform.position = new Vector3(avatars[3].transform.position.x, selector.transform.position.y);
     }
 
     public void OnWheelBarrelSelect()
     {
         GameInformation.playerAvatar = "WHEELBARREL";
+        selector.transform.position = new Vector3(avatars[4].transform.position.x, selector.transform.position.y);
     }
 }
