3cfb4f754ab24506f716c551decb7f2f26b1f37e
Gus McCoy <51655110+gusmccoy@users.noreply.github.com>
2021-03-26 16:56:04 -0500

    Arrow Animation

--- Assets/Scripts/ArrowController.cs
+++ Assets/Scripts/ArrowController.cs
@@ -14,32 +14,13 @@ public class ArrowController : MonoBehaviour
 
     public float Speed = 5f;
 
-    private bool northBound = false;
-    void Start()
-    {
-        gameObject.SetActive(false);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        //if ((transform.position.x <= 10))
-        //{
-        //    transform.position = new Vector3(transform.position.x - 1f, transform.position.y);
-        //}
-        //else if (transform.position.x >= 0)
-        //{
-        //    transform.position = new Vector3(transform.position.x + 1f, transform.position.y);
-        //}
-    }
-
     public void EnableArrow(float sX, float sY, float mY, float mX, float rX, float rY, float rZ, float speed)
     {
         gameObject.SetActive(true);
         // MaxPostitionX = mX;
         // MaxPostitionY = mY;
-        transform.position = new Vector3(sX, sY);
-        transform.eulerAngles = new Vector3(0, 0, rZ);
+        // transform.position = new Vector3(sX, sY);
+        // transform.eulerAngles = new Vector3(0, 0, rZ);
         // Speed = speed;
     }
 
--- Assets/Scripts/MainMenuController.cs
+++ Assets/Scripts/MainMenuController.cs
@@ -4,20 +4,44 @@ using UnityEngine;
 
 public class MainMenuController : MonoBehaviour
 {
-
-    public GameObject suggestionArrow;
     public GameObject tutorialPrompt;
+    public ArrowController suggestionArrow;
+
+    public bool forward = true; 
 
     void Start()
     {
-        suggestionArrow.SetActive(false);
+        suggestionArrow.DisableArrow();
         tutorialPrompt.SetActive(false);
 
         if (!GameInformation.firstPlayComplete)
         {
-            suggestionArrow.SetActive(true);
+            suggestionArrow.gameObject.SetActive(true);
             tutorialPrompt.SetActive(true);
+            StartCoroutine(MoveForward());
         }
 
+    }
+
+    IEnumerator MoveForward()
+    {
+        for (float ft = 1f; ft >= 0; ft -= 0.1f)
+        {
+            suggestionArrow.gameObject.transform.position = new Vector3(suggestionArrow.gameObject.transform.position.x + 10f, suggestionArrow.gameObject.transform.position.y);
+            yield return new WaitForSeconds(.1f);
+        }
+
+        StartCoroutine(MoveBackward());
+    }
+
+    IEnumerator MoveBackward()
+    {
+        for (float ft = 1f; ft >= 0; ft -= 0.1f)
+        {
+            suggestionArrow.gameObject.transform.position = new Vector3(suggestionArrow.gameObject.transform.position.x - 10f, suggestionArrow.gameObject.transform.position.y);
+            yield return new WaitForSeconds(.1f);
+        }
+
+        StartCoroutine(MoveForward());
     }
 }
