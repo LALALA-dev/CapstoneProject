18f4d4ee0c48e703f9d41e1a760b6e0b706f5096
Manerd Humhordun <s.heap3927@gmail.com>
2021-03-28 00:33:11 -0500

    Resized Longest Network Card

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -576,14 +576,14 @@ public class GameManager : MonoBehaviour
             longestNetworkPlayerText.text = "Player One";
             longestNetworkLengthText.text = GameInformation.playerOneNetwork.ToString() + " Roads";
             longestNetworkMessage.SetActive(true);
-            longestNetworkMessage.transform.position = new Vector3(642.1f, 930.6f, 0f);
+            longestNetworkMessage.transform.position = new Vector3(545f, 860f, 0f);
         }
         else if (GameInformation.playerTwoNetwork > GameInformation.playerOneNetwork)
         {
             longestNetworkPlayerText.text = "Player Two";
             longestNetworkLengthText.text = GameInformation.playerTwoNetwork.ToString() + " Roads";
             longestNetworkMessage.SetActive(true);
-            longestNetworkMessage.transform.position = new Vector3(1277.9f, 930.6f, 0f);
+            longestNetworkMessage.transform.position = new Vector3(1360f, 860f, 0f);
         }
         else
         {
