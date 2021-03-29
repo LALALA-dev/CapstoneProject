0dd004233b3496f18026a2e857fd5a7e61954489
Gus McCoy <51655110+gusmccoy@users.noreply.github.com>
2021-03-29 12:12:41 -0500

    waitingAnimation

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -358,6 +358,7 @@ public class GameManager : MonoBehaviour
         if(!GameInformation.playerIsHost)
         {
             currentPlayerMessage.text = "AI's Move";
+            waitingAnimation.SetActive(true);
             BoardState AIMove = beginnerAI.MakeRandomOpeningMove(gameController.getGameBoard().getBoardState());
             gameController.getGameBoard().setBoard(AIMove.squareStates, AIMove.nodeStates, AIMove.branchStates);
             EndCurrentAIPlayersTurn();
@@ -365,6 +366,7 @@ public class GameManager : MonoBehaviour
         else
         {
             currentPlayerMessage.text = "Your Move";
+            waitingAnimation.SetActive(false);
         }
     }
 
@@ -393,6 +395,7 @@ public class GameManager : MonoBehaviour
                 if(turnNumber == 1)
                 {
                     currentPlayerMessage.text = "AI's Move";
+                    waitingAnimation.SetActive(true);
                     turnNumber++;
                     RandomAIOpeningMove();
 
@@ -402,6 +405,7 @@ public class GameManager : MonoBehaviour
                 else if(turnNumber == 4)
                 {
                     currentPlayerMessage.text = "AI's Move";
+                    waitingAnimation.SetActive(true);
                     GameInformation.openingSequence = false;
                     turnNumber++;
                     GameInformation.currentPlayer = "AI"; 
@@ -428,6 +432,7 @@ public class GameManager : MonoBehaviour
                 if(turnNumber == 2)
                 {
                     currentPlayerMessage.text = "Your Move";
+                    waitingAnimation.SetActive(false);
                     turnNumber++;
                     gameController.FlipColors();
                     BeginHumanOpeningMove();
@@ -435,6 +440,7 @@ public class GameManager : MonoBehaviour
                 else if(turnNumber == 3)
                 {
                     currentPlayerMessage.text = "AI's Move";
+                    waitingAnimation.SetActive(true);
                     turnNumber++;
                     RandomAIOpeningMove();
                     EndCurrentAIPlayersTurn();
@@ -452,11 +458,13 @@ public class GameManager : MonoBehaviour
                     boardManager.RefreshBoardGUI();
                     turnNumber++;
                     currentPlayerMessage.text = "Your Move";
+                    waitingAnimation.SetActive(false);
                     BeginHumanOpeningMove();
                 }
                 else if (turnNumber == 4)
                 {
                     currentPlayerMessage.text = "Your Move";
+                    waitingAnimation.SetActive(false);
                     GameInformation.openingSequence = false;
                     turnNumber++;
                     GameInformation.currentPlayer = "HUMAN";
@@ -474,6 +482,7 @@ public class GameManager : MonoBehaviour
                 if (turnNumber == 2)
                 {
                     currentPlayerMessage.text = "AI's Move";
+                    waitingAnimation.SetActive(true);
                     turnNumber++;
                     RandomAIOpeningMove();
                     GameInformation.humanMoveFinished = false;
@@ -482,6 +491,7 @@ public class GameManager : MonoBehaviour
                 else if (turnNumber == 3)
                 {
                     currentPlayerMessage.text = "Your Move";
+                    waitingAnimation.SetActive(false);
                     turnNumber++;
                     GameInformation.currentPlayer = "HUMAN";
                     gameController.FlipColors();
