8c2bbea82236147847f6a2a2a625db2ee3d9d517
Manerd Humhordun <s.heap3927@gmail.com>
2021-04-01 02:26:10 -0500

    Make help panels appear on request and navigable.
    The help button now activates the help popups. Arrows navigate between panels and dynamically appear and disappear when at the panels' beginning and end. X button closes popup.

--- Assets/Scripts/HelpManager.cs
+++ Assets/Scripts/HelpManager.cs
@@ -4,19 +4,80 @@ using UnityEngine;
 
 public class HelpManager : MonoBehaviour
 {
+    private int currentPanel;
+    // The order in which the help panels should be displayed.
+    //  Changes as the game advances. 
+    private int[] panelOrderKey;
+    public GameObject[] helpPanels;
+    public GameObject nextButton;
+    public GameObject previousButton;
+    public GameObject cancelButton;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        panelOrderKey = new int[helpPanels.Length];
+
+        for (int i = 0; i < helpPanels.Length; ++i)
+        {
+            panelOrderKey[i] = i;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    // TODO: Might also need to deactivate certain aspects of the board &| buttons.
+    public void onHelpClick()
     {
+        // Activate the first help panel that should be shown.
+        helpPanels[panelOrderKey[0]].SetActive(true);
+        currentPanel = 0;
         
+        if (panelOrderKey[1] != null)
+        {
+            nextButton.SetActive(true);
+        }
+        cancelButton.SetActive(true);
     }
 
-    void onHelpClick()
+    public void onNextHelp()
+    {
+        helpPanels[panelOrderKey[currentPanel]].SetActive(false);
+        helpPanels[panelOrderKey[++currentPanel]].SetActive(true);
+
+        previousButton.SetActive(true);
+
+        if ((currentPanel + 1) == panelOrderKey.Length || panelOrderKey[currentPanel + 1] == null)
+        {
+            nextButton.SetActive(false);
+        }
+    }
+
+    public void onPreviousHelp()
+    {
+        helpPanels[currentPanel].SetActive(false);
+        helpPanels[--currentPanel].SetActive(true);
+
+        nextButton.SetActive(true);
+
+        if (currentPanel == 0)
+        {
+            previousButton.SetActive(false);
+        }
+    }
+
+    public void onCancelHelp()
+    {
+        helpPanels[currentPanel].SetActive(false);
+        nextButton.SetActive(false);
+        previousButton.SetActive(false);
+        cancelButton.SetActive(false);
+    }
+
+    // TODO: Set up to change the ordering of the panels based on current turn.
+    //  Example:
+    //      0 and 1 for opening moves.
+    //      1, 2, and 3 for moves 4-6|8.
+    //      3, 1, and 2 for moves > 6|8.
+    public void changePanelOrder()
     {
 
     }
