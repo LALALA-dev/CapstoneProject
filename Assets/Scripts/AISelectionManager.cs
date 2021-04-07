da62a2d4257b2a506869c0532767aa34a35bd0ee
Gus McCoy <51655110+gusmccoy@users.noreply.github.com>
2021-04-07 08:44:06 -0500

    Revert "Revert "Merge branch 'Development' of https://github.com/ManerdHumhordun/Capstone-Spring-2021 into Development""
    This reverts commit 9d1a526cb0211d9fb8760ec94dd59097bab23b05.

--- Assets/Scripts/AISelectionManager.cs
+++ Assets/Scripts/AISelectionManager.cs
@@ -23,7 +23,6 @@ public class AISelectionManager : MonoBehaviour
 
     public void OnGoSelect()
     {
-        Debug.Log("OOooohhh, I'm a log statement!");
         // Assign a random avatar to the AI that is not selected by player.
         int aiAvatarID;
         do
